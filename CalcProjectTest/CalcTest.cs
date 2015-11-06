82d6f0d09b3a2f5bb440d5a53d714fe18926252d
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2015-11-06 13:20:37 +0300

    add projects with tests

---
+++ CalcProjectTest/CalcTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CalcProject;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalcProjectTest
+{
+    [TestClass]
+    public class CalcTest
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            Assert.AreEqual(5, Calculator.plus(3, 2));
+        }
+    }
+}
