fab042df924bd485e1bc26e91b9d9b585e466799
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2015-11-06 12:10:26 +0300

    add simple project

---
+++ CalcProject/Calculator.cs
@@ -0,0 +1,15 @@
+﻿namespace CalcProject
+{
+    public class Calculator
+    {
+        public static double plus(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double minus(double a, double b)
+        {
+            return a - b;
+        }
+    }
+}
