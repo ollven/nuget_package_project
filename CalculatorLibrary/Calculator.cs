1cd2b361cfbf1d62101c151c6a407d1cd9af3c01
Nadia Burnasheva <nadezhda.burnasheva@jetbrains.com>
2021-07-16 10:03:20 +0200

    initial commit

---
+++ CalculatorLibrary/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+	public static double plus(double a, double b)
+        {
+            
+            return a + b;
+            
+        }
+        
+        public static double minus(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
