196ed13e63767e12fc4bc13f20365ab7fb338030
burnasheva <nadez.hda.burnasheva@gmail.com>
2021-07-20 15:54:01 +0200

    add library with 2 dots in the name

---
+++ Calc.Project.Library/Calc.Project.Library/Calc.Project.Library/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calc.Project.Library
+{
+    public class Calculator
+    {
+        public static int plus(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
