25d059b293f7319e2f1c053c213f8ec7aeaeceff
ollven <ollven@TeamCity>
2023-09-27 15:07:28 +0200

    TeamCity change in 'Nuget Package Project' project: Artifacts storage settings updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -54,6 +54,7 @@ project {
             connectionId = "AmazonWebServicesAws"
             bucketName = "ollven-test"
             forceVirtualHostAddressing = true
+            verifyIntegrityAfterUpload = true
         }
         activeStorage {
             id = "PROJECT_EXT_21"
